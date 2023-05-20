cd5d9634b7938023bfba07ed75fec5574ef9367a
Darlon <darlonv@gmail.com>
2023-05-20 10:14:07 -0300

    content added

--- src/components/Bib.jsx
+++ src/components/Bib.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 export default function Bib({bib, page, type, inline=false}) {
   let base = {
-    ['tanenbaum_2011']: {['cite']: 'TANENBAUM 2011', ['abnt']:'TANENBAUM, A.S., Wetherall, D. Redes de Computadores. 5. ed. São Paulo: Pearson do Brasil, 2011.'}
+    ['tanenbaum_2011_book']: {['cite']: 'TANENBAUM 2011', ['abnt']:'TANENBAUM, A.S., Wetherall, D. Redes de Computadores. 5. ed. São Paulo: Pearson do Brasil, 2011.'},
+    ['kurose_2013_book']   : {['cite']: 'KUROSE 2013', ['abnt']:'KUROSE, Jim; ROSS, Keith W. Redes de computadores e a internet: uma abordagem top-down. 6.ed. São Paulo: Pearson, 2013. ISBN 9788581436777.'},
+    ['torres_2009_book']: {['cite']: 'TORRES 2009', ['abnt']:'TORRES, G. Redes de Computadores. Rio de Janeiro: Novaterra, 2009. ISBN 978-85-61893-05-7.'},
   }
 
   let data;

1dbdf3fdbfb39e94d913ecb4712645a075f5c7a1
Darlon <darlonv@gmail.com>
2023-05-20 10:43:45 -0300

    link support

--- src/components/Bib.jsx
+++ src/components/Bib.jsx
@@ -1,18 +1,32 @@
 
 import React from 'react';
 
-export default function Bib({bib, page, type, inline=false}) {
+export default function Bib({bib, page, type, inline=false, link=false}) {
   let base = {
-    ['tanenbaum_2011_book']: {['cite']: 'TANENBAUM 2011', ['abnt']:'TANENBAUM, A.S., Wetherall, D. Redes de Computadores. 5. ed. São Paulo: Pearson do Brasil, 2011.'},
-    ['kurose_2013_book']   : {['cite']: 'KUROSE 2013', ['abnt']:'KUROSE, Jim; ROSS, Keith W. Redes de computadores e a internet: uma abordagem top-down. 6.ed. São Paulo: Pearson, 2013. ISBN 9788581436777.'},
-    ['torres_2009_book']: {['cite']: 'TORRES 2009', ['abnt']:'TORRES, G. Redes de Computadores. Rio de Janeiro: Novaterra, 2009. ISBN 978-85-61893-05-7.'},
+    ['tanenbaum_2011_book']: {
+      ['cite']: 'TANENBAUM 2011', 
+      ['abnt']: 'TANENBAUM, A.S., Wetherall, D. Redes de Computadores. 5. ed. São Paulo: Pearson do Brasil, 2011.', 
+      ['link']: 'https://plataforma.bvirtual.com.br/Acervo/Publicacao/2610',
+      
+    },
+    ['kurose_2013_book']   : {
+      ['cite']: 'KUROSE 2013',    
+      ['abnt']: 'KUROSE, Jim; ROSS, Keith W. Redes de computadores e a internet: uma abordagem top-down. 6.ed. São Paulo: Pearson, 2013. ISBN 9788581436777.',
+      ['link']: 'https://plataforma.bvirtual.com.br/Acervo/Publicacao/198909',
+    },
+    ['torres_2009_book']   : {
+      ['cite']: 'TORRES 2009',    
+      ['abnt']: 'TORRES, G. Redes de Computadores. Rio de Janeiro: Novaterra, 2009. ISBN 978-85-61893-05-7.',
+    },
   }
 
   let data;
 
-  
+  //Verifica se é referência ou bibliografia
   if(!type){
-    //citação
+    //referência
+
+    //Verifica se é necessário incluir a página
     if(!page){
       //sem página
       data = base[bib]['cite'];
@@ -22,14 +36,19 @@ export default function Bib({bib, page, type, inline=false}) {
       data = base[bib]['cite']+', pg. '+page;
     }
     
+    //Verifica se é referência inline
     if(!inline){
-      //coloca parênteses antes e depois
+      //Caso não seja inline, coloca parênteses antes e depois
       data = '('+data+')';
     }
     
   }else{
-    //referência
+    //Bibliografia
     data = base[bib][type]
+    if(link){
+      //Bibliografia com link
+      data = <a href={base[bib]['link']} target='_blank'>{data}</a>
+    }
 
   }
   
